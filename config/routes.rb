7efe4c1497b692470b12d3295117d8caa1a406a9
syn3r6y <syn3rgy@live.ca>
2017-09-23 13:31:05 -0600

    creates template, basic form, action, route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get 'about', to: 'pages#about'
-
+  # Gives a new article path with CRUD paths
+  resources :articles
 end
